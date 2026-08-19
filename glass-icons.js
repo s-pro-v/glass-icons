735672366de02cf61f838ee98d7e076ff96610c6
s-pro-v <146633771+s-pro-v@users.noreply.github.com>
2026-08-19 02:49:41 +0200

    Add GlassIcons functionality for SVG icons

---
+++ glass-icons.js
@@ -0,0 +1,83 @@
+(function (global) {
+    // Baza ikon (wklej tutaj resztę z pliku glass_icons.json)
+    const icons = {
+        "sliders-vertical": `<svg width="48" height="48" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" class="nc-icon-wrapper">
+                <path d="M4 2C4.55228 2 5 2.44772 5 3V21C5 21.5523 4.55228 22 4 22C3.44772 22 3 21.5523 3 21V3C3 2.44772 3.44772 2 4 2ZM12 2C12.5523 2 13 2.44772 13 3V21C13 21.5523 12.5523 22 12 22C11.4477 22 11 21.5523 11 21V3C11 2.44772 11.4477 2 12 2ZM20 2C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22C19.4477 22 19 21.5523 19 21V3C19 2.44772 19.4477 2.00001 20 2Z" fill="url(#1783337238357-6639607_sliders-3-vertical_existing_0_s9e74zc74)" data-glass="origin" mask="url(#1783337238357-6639607_sliders-3-vertical_mask_zc3ose35h)"></path>
+                <path d="M4 2C4.55228 2 5 2.44772 5 3V21C5 21.5523 4.55228 22 4 22C3.44772 22 3 21.5523 3 21V3C3 2.44772 3.44772 2 4 2ZM12 2C12.5523 2 13 2.44772 13 3V21C13 21.5523 12.5523 22 12 22C11.4477 22 11 21.5523 11 21V3C11 2.44772 11.4477 2 12 2ZM20 2C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22C19.4477 22 19 21.5523 19 21V3C19 2.44772 19.4477 2.00001 20 2Z" fill="url(#1783337238357-6639607_sliders-3-vertical_existing_0_s9e74zc74)" data-glass="clone" filter="url(#1783337238357-6639607_sliders-3-vertical_filter_2y1ka8tps)" clip-path="url(#1783337238357-6639607_sliders-3-vertical_clipPath_gjzvp5y9p)"></path>
+                <path d="M12 6C13.3807 6 14.5 7.11929 14.5 8.5V15.5C14.5 16.8807 13.3807 18 12 18C10.6193 18 9.5 16.8807 9.5 15.5V8.5C9.5 7.11929 10.6193 6 12 6ZM4 10C5.38071 10 6.5 11.1193 6.5 12.5V14.5C6.5 15.8807 5.38071 17 4 17C2.61929 17 1.5 15.8807 1.5 14.5V12.5C1.5 11.1193 2.61929 10 4 10ZM20 4C21.3807 4 22.5 5.11929 22.5 6.5V9.5C22.5 10.8807 21.3807 12 20 12C18.6193 12 17.5 10.8807 17.5 9.5V6.5C17.5 5.11929 18.6193 4 20 4Z" fill="url(#1783337238357-6639607_sliders-3-vertical_existing_1_j8y1g0cxw)" data-glass="blur"></path>
+                <path d="M13.75 15.5V8.5C13.75 7.5335 12.9665 6.75 12 6.75C11.0335 6.75 10.25 7.5335 10.25 8.5V15.5C10.25 16.4665 11.0335 17.25 12 17.25V18C10.6193 18 9.5 16.8807 9.5 15.5V8.5C9.5 7.11929 10.6193 6 12 6C13.3807 6 14.5 7.11929 14.5 8.5V15.5C14.5 16.8807 13.3807 18 12 18V17.25C12.9665 17.25 13.75 16.4665 13.75 15.5Z" fill="url(#1783337238357-6639607_sliders-3-vertical_existing_2_3q66e5lo6)"></path>
+                <path d="M5.75 14.5V12.5C5.75 11.5335 4.9665 10.75 4 10.75C3.0335 10.75 2.25 11.5335 2.25 12.5V14.5C2.25 15.4665 3.0335 16.25 4 16.25V17C2.61929 17 1.5 15.8807 1.5 14.5V12.5C1.5 11.1193 2.61929 10 4 10C5.38071 10 6.5 11.1193 6.5 12.5V14.5C6.5 15.8807 5.38071 17 4 17V16.25C4.9665 16.25 5.75 15.4665 5.75 14.5Z" fill="url(#1783337238357-6639607_sliders-3-vertical_existing_3_j2ansn7x1)"></path>
+                <path d="M21.75 9.5V6.5C21.75 5.5335 20.9665 4.75 20 4.75C19.0335 4.75 18.25 5.5335 18.25 6.5V9.5C18.25 10.4665 19.0335 11.25 20 11.25V12C18.6193 12 17.5 10.8807 17.5 9.5V6.5C17.5 5.11929 18.6193 4 20 4C21.3807 4 22.5 5.11929 22.5 6.5V9.5C22.5 10.8807 21.3807 12 20 12V11.25C20.9665 11.25 21.75 10.4665 21.75 9.5Z" fill="url(#1783337238357-6639607_sliders-3-vertical_existing_4_xv8cve4zy)"></path>
+                <defs>
+                    <linearGradient id="1783337238357-6639607_sliders-3-vertical_existing_0_s9e74zc74" x1="12" y1="2" x2="12" y2="22" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#575757"></stop>
+                        <stop offset="1" stop-color="#151515"></stop>
+                    </linearGradient>
+                    <linearGradient id="1783337238357-6639607_sliders-3-vertical_existing_1_j8y1g0cxw" x1="12" y1="4" x2="12" y2="18" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#E3E3E599"></stop>
+                        <stop offset="1" stop-color="#BBBBC099"></stop>
+                    </linearGradient>
+                    <linearGradient id="1783337238357-6639607_sliders-3-vertical_existing_2_3q66e5lo6" x1="12" y1="6" x2="12" y2="12.949" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#fff"></stop>
+                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                    </linearGradient>
+                    <linearGradient id="1783337238357-6639607_sliders-3-vertical_existing_3_j2ansn7x1" x1="4" y1="10" x2="4" y2="14.054" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#fff"></stop>
+                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                    </linearGradient>
+                    <linearGradient id="1783337238357-6639607_sliders-3-vertical_existing_4_xv8cve4zy" x1="20" y1="4" x2="20" y2="8.633" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#fff"></stop>
+                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                    </linearGradient>
+                    <filter id="1783337238357-6639607_sliders-3-vertical_filter_2y1ka8tps" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse">
+                        <feGaussianBlur stdDeviation="2" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur>
+                    </filter>
+                    <clipPath id="1783337238357-6639607_sliders-3-vertical_clipPath_gjzvp5y9p">
+                        <path d="M12 6C13.3807 6 14.5 7.11929 14.5 8.5V15.5C14.5 16.8807 13.3807 18 12 18C10.6193 18 9.5 16.8807 9.5 15.5V8.5C9.5 7.11929 10.6193 6 12 6ZM4 10C5.38071 10 6.5 11.1193 6.5 12.5V14.5C6.5 15.8807 5.38071 17 4 17C2.61929 17 1.5 15.8807 1.5 14.5V12.5C1.5 11.1193 2.61929 10 4 10ZM20 4C21.3807 4 22.5 5.11929 22.5 6.5V9.5C22.5 10.8807 21.3807 12 20 12C18.6193 12 17.5 10.8807 17.5 9.5V6.5C17.5 5.11929 18.6193 4 20 4Z" fill="url(#1783337238357-6639607_sliders-3-vertical_existing_1_j8y1g0cxw)"></path>
+                    </clipPath>
+                    <mask id="1783337238357-6639607_sliders-3-vertical_mask_zc3ose35h">
+                        <rect width="100%" height="100%" fill="#FFF"></rect>
+                        <path d="M12 6C13.3807 6 14.5 7.11929 14.5 8.5V15.5C14.5 16.8807 13.3807 18 12 18C10.6193 18 9.5 16.8807 9.5 15.5V8.5C9.5 7.11929 10.6193 6 12 6ZM4 10C5.38071 10 6.5 11.1193 6.5 12.5V14.5C6.5 15.8807 5.38071 17 4 17C2.61929 17 1.5 15.8807 1.5 14.5V12.5C1.5 11.1193 2.61929 10 4 10ZM20 4C21.3807 4 22.5 5.11929 22.5 6.5V9.5C22.5 10.8807 21.3807 12 20 12C18.6193 12 17.5 10.8807 17.5 9.5V6.5C17.5 5.11929 18.6193 4 20 4Z" fill="#000"></path>
+                    </mask>
+                </defs>
+            </g>
+        </svg>`,
+        
+        // Dodaj tu resztę ikon z pliku JSON (klucz to nazwa bez .svg)
+    };
+
+    const GlassIcons = {
+        replace: function () {
+            const elements = document.querySelectorAll('[data-glass-icon]');
+            
+            elements.forEach(el => {
+                const iconName = el.getAttribute('data-glass-icon');
+                if (icons[iconName]) {
+                    // Zmiana statycznych kolorów na zmienne CSS
+                    let svgCode = icons[iconName]
+                        .replace(/stop-color="#575757"/gi, 'stop-color="var(--normal-color-start)"')
+                        .replace(/stop-color="#151515"/gi, 'stop-color="var(--normal-color-end)"')
+                        .replace(/stop-color="#E3E3E599"/gi, 'stop-color="var(--glass-color-start)"')
+                        .replace(/stop-color="#BBBBC099"/gi, 'stop-color="var(--glass-color-end)"');
+                    
+                    el.innerHTML = svgCode;
+                    
+                    const svgEl = el.querySelector('svg');
+                    if (svgEl) {
+                        svgEl.setAttribute('class', `glass-icon ${el.className}`);
+                    }
+                } else {
+                    console.warn(`GlassIcons: Nie znaleziono ikony "${iconName}"`);
+                }
+            });
+        }
+    };
+
+    global.GlassIcons = GlassIcons;
+
+    document.addEventListener("DOMContentLoaded", () => {
+        GlassIcons.replace();
+    });
+
+})(window);
